4b218bea1d2828eda0e35e57f59ee23072889529
PleBea <op@plebea.com>
2024-03-27 17:52:32 +0000

    apply env

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -23,7 +24,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb url"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGODB_URL")))
 	handleError(err)
 
 	uesrsCollection := client.Database("test").Collection("user")
