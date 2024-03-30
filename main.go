a6d736d517853f16bb530ba36c1e3fb6b8fb5d10
PleBea <op@plebea.com>
2024-03-30 12:04:35 +0000

    impl redis

--- main.go
+++ main.go
@@ -2,90 +2,57 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"log"
+	"crypto/tls"
 	"net/http"
-	"os"
-	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
 
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/redis/go-redis/v9"
 )
 
-
-func handleError(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-type DatabaseConfig struct {
-	Username            string `json:"username"`
-	Password            string `json:"password"`
-	Engine              string `json:"engine"`
-	Host                string `json:"host"`
-	Port                int64    `json:"port"`
-	Ssl                 bool   `json:"ssl"`
-}
-
-func loadEnvironment() (DatabaseConfig) {
-	stringConfig := os.Getenv("db")
-	var config DatabaseConfig
-
-	err := json.Unmarshal([]byte(stringConfig), &config)
-	handleError(err)
-
-	return config 
-}
-
 func main() {
-	config := loadEnvironment()
-
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 
-	uri := "mongodb://" + config.Username + ":" + config.Password + "@" + config.Host + ":" + strconv.FormatInt(config.Port, 10) + "/?ssl=" + strconv.FormatBool(config.Ssl)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
-	handleError(err)
-
-	uesrsCollection := client.Database("test").Collection("user")
-
 	r := gin.Default()
-	r.GET("/hello", func(c *gin.Context) {
-		cursor, _ := uesrsCollection.Find(context.Background(), bson.M{})
-		defer cursor.Close(context.Background())
 
-		var result []*bson.M
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "srn-redis-4isy4c.serverless.apn2.cache.amazonaws.com:6379",
+		Password: "",
+		DB: 0,
+		TLSConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	})
 
-		for cursor.Next(context.Background()) {
-			var elem bson.M
-			err := cursor.Decode(&elem)
-			handleError(err)
-			result = append(result, &elem)
-		}
-		err := cursor.Err();
-		handleError(err)
 
+	r.GET("/world", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"user": result,
+			"message": "world",
 		})
 	})
 
-	r.GET("/hello/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		_, err := uesrsCollection.InsertOne(context.Background(), bson.M{
-			name: name,
-		})
-		handleError(err)
+	r.GET("/world/cache/:value", func (c *gin.Context)  {
+		status := "get cache"
 
+		value := c.Param("value")
+
+		res, err := rdb.Get(ctx, value).Result()
+		if err == redis.Nil {
+			err := rdb.Set(ctx, value, len(value), 3600*time.Second).Err()
+			if err != nil {
+				panic(err)
+			}
+			status = "set cache"
+		}  else if err != nil {
+			panic(err)
+		} 
+		
 		c.JSON(http.StatusOK, gin.H{
-			"message": name,
+			"message": res,
+			"stutus": status,
 		})
 	})
-
 	r.Run()
 }
