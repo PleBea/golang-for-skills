094cfacbfa98e2c0e916a96f1c2270be75e4dddb
PleBea <bini060315kim@gmail.com>
2024-03-31 18:22:04 +0900

    default

--- main.go
+++ main.go
@@ -1,107 +1,16 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"log"
 	"net/http"
-	"strconv"
-	"time"
 
 	"github.com/gin-gonic/gin"
-
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
 )
 
-
-func handleError(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-type DatabaseConfig struct {
-	Username            string `json:"username"`
-	Password            string `json:"password"`
-	Engine              string `json:"engine"`
-	Host                string `json:"host"`
-	Port                int64    `json:"port"`
-	Ssl                 bool   `json:"ssl"`
-}
-
-func loadSecretManager() (DatabaseConfig) {
-	secretName := "db_access"
-	region := "ap-northeast-2"
-	config, err := config.LoadDefaultConfig(context.Background(), config.WithRegion(region))
-	handleError(err)
-
-	svc := secretsmanager.NewFromConfig(config)
-
-	input := &secretsmanager.GetSecretValueInput{
-		SecretId: aws.String(secretName),
-	}
-
-	result, err := svc.GetSecretValue(context.TODO(), input)
-	handleError(err)
-
-	var secretString string = *result.SecretString
-
-	var secret DatabaseConfig
-
-	err = json.Unmarshal([]byte(secretString), &secret)
-	handleError(err)
-
-	return secret
-}
-
 func main() {
-	config := loadSecretManager()
-	
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-	defer cancel()
-
-	uri := "mongodb://" + config.Username + ":" + config.Password + "@" + config.Host + ":" + strconv.FormatInt(config.Port, 10) + "/?ssl=" + strconv.FormatBool(config.Ssl)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
-	handleError(err)
-
-	uesrsCollection := client.Database("test").Collection("user")
-
 	r := gin.Default()
 	r.GET("/hello", func(c *gin.Context) {
-		cursor, _ := uesrsCollection.Find(context.Background(), bson.M{})
-		defer cursor.Close(context.Background())
-
-		var result []*bson.M
-
-		for cursor.Next(context.Background()) {
-			var elem bson.M
-			err := cursor.Decode(&elem)
-			handleError(err)
-			result = append(result, &elem)
-		}
-		err := cursor.Err();
-		handleError(err)
-
 		c.JSON(http.StatusOK, gin.H{
-			"user": result,
-		})
-	})
-
-	r.GET("/hello/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		_, err := uesrsCollection.InsertOne(context.Background(), bson.M{
-			name: name,
-		})
-		handleError(err)
-
-		c.JSON(http.StatusOK, gin.H{
-			"message": name,
+			"message": "hello",
 		})
 	})
 
