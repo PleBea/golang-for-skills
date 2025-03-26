71d28742fed78bc7e52d4edbb1f2624aef777d5a
sbkim <sbkim@enki.co.kr>
2025-03-26 14:29:25 +0900

    good

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
 func main() {
@@ -12,6 +13,10 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "hello",
 		})
+	})
+
+	r.GET("/db-test", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"DB_HOST": os.Getenv("DB_HOST"), "DB_PORT": os.Getenv("DB_PORT"), "DB_USER": os.Getenv("DB_USER"), "DB_PASSWORD": os.Getenv("DB_PASSWORD"), "DB_NAME": os.Getenv("DB_NAME")})
 	})
 
 	r.Run()
